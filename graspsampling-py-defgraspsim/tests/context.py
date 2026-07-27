282a42589f0ede2924d87bb491ca7481be7cbcbb
Andrew White <andrewh@cdw.com>
2026-07-27 07:29:45 -0500

    fix: import graspsampling before adding project root to sys.path

--- graspsampling-py-defgraspsim/tests/context.py
+++ graspsampling-py-defgraspsim/tests/context.py
@@ -19,8 +19,9 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 """Set the proper path to the graspsampling module."""
 
-import graspsampling
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import graspsampling
